9092bd0306073739210ad7bc8ea9830a829d299b
Kshitij Ingale <kshitijingale2@gmail.com>
2021-11-26 16:53:38 +0530

    update port

--- app.js
+++ app.js
@@ -131,6 +131,6 @@ app.get("/about", function (req, res) {
     res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port 3000");
 });
